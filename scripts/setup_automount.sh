0122cc65f7f0fb8865cd46846969b8668a4b8440
Jordan Evens <jordan.evens@NRCan-RNCan.gc.ca>
2024-03-28 16:02:43 -0400

    removed setting chown twice on /mnt/data

--- scripts/setup_automount.sh
+++ scripts/setup_automount.sh
@@ -5,8 +5,6 @@ echo "Setting up automount from alpine distros"
 sudo mkdir -p /mnt/data
 # need this if docker isn't installed yet
 sudo mkdir -p /var/lib/docker
-# set up directories for mounting
-sudo chown -R ${USER}:${USER} /mnt/data
 
 # set up service to start alpine containers for mount points
 sudo cp vhdx_mount.sh /usr/local/sbin/
